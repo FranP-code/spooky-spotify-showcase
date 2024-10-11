9a245129e7a9a20b601c98105a9d4f9c284502ba
Francisco Pessano <franpessano1@gmail.com>
2024-10-10 23:52:56 -0300

    Fix growing on albums showcase

--- src/app/_components/album-showcase.tsx
+++ src/app/_components/album-showcase.tsx
@@ -15,7 +15,7 @@ export default function AlbumShowcase({
       className="flex w-full flex-wrap gap-3 rounded-xl bg-white bg-opacity-10 p-5 backdrop-blur-lg backdrop-filter"
     >
       <img className="h-36 w-36 rounded" src={imageSource} alt={album.name} />
-      <section className="flex h-36 flex-col">
+      <section className="flex h-36 flex-grow flex-col">
         <h4 className="mb-2 text-lg font-semibold leading-none">
           {album.name}
         </h4>
