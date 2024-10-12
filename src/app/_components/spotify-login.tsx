c5148f6de6d88d58b33484e9ac3a88e32144d4c1
Francisco Pessano <franpessano1@gmail.com>
2024-10-11 22:17:12 -0300

    Login button

--- src/app/_components/spotify-login.tsx
+++ src/app/_components/spotify-login.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { MdiSpotify } from "./spotify-icon";
+
 export default function SpotifyLogin() {
   const loginToSpotify = () => {
     window.location.href = "/api/spotify-login";
@@ -7,9 +9,10 @@ export default function SpotifyLogin() {
 
   return (
     <button
-      className="rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700"
+      className="flex items-center justify-center gap-1 rounded-full bg-green-500 py-2 pl-4 pr-5 font-bold text-white hover:bg-green-700"
       onClick={loginToSpotify}
     >
+      <MdiSpotify className="h-10 w-10" />
       Login to Spotify
     </button>
   );
